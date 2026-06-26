a2c447283b6cffe503389358c77e2dcb7963f960
Suelen Francisco Marques <suelen-marques@outlook.com.br>
2026-06-25 22:04:09 -0300

    refactor(tests): padroniza nomenclatura do Checkout para padrão

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,40 +1,46 @@
 import Login from '../pages/login'
-import Checkout from "../pages/checkout"
-import Inventory from "../pages/inventory"
-import Header from "../pages/header"
-import Cart from "../pages/cart"
+import Checkout from '../pages/checkout'
+import Inventory from '../pages/inventory'
+import Header from '../pages/header'
+import Cart from '../pages/cart'
+import { DATA } from '../support/data'
 
-
-describe('checkout', () =>{
+describe('Checkout - Preenchimento e Validação de Formulário', () => {
 
     beforeEach(() => {
-        Login.visitarPagina()
+        Login.acessarPaginaDeLogin()
         Login.preencherCredenciaisValidas()
+
+        Inventory.adicionarProdutoAoCarrinho(DATA.produto)
+
+        Header.navegarParaCarrinho()
+
+        Cart.iniciarCheckout()
     })
 
-    it('Finalidar compra', () => {
-        Inventory.adicionarProduto('Sauce Labs Backpack')
-        
-        Header.navegarParaCarrinho()
-        
-        Cart.irParaCheckout()
-
-        Checkout.preencherInformacoesValidas()   
-        Checkout.continuar()
-        Checkout.finalizarCompra()               
-        Checkout.validarSucesso()               
+    it('Finalizar a compra com sucesso ao preencher informações válidas', () => {
+        Checkout.preencherInformacoesDeEntrega()
+        Checkout.avancarParaResumo()
+        Checkout.finalizarCompra()
+        Checkout.validarConfirmacaoDePedido()
     })
 
-    it.only('Nome vazio', () => {
-        Inventory.adicionarProduto('Sauce Labs Backpack')
-        
-        Header.navegarParaCarrinho()
-        
-        Cart.irParaCheckout()
+    it('Exibir erro ao tentar avançar com o primeiro nome em branco', () => {
+        Checkout.submeterFormularioSemPrimeiroNome()
+        Checkout.avancarParaResumo()
+        Checkout.validarMensagemDeErroPrimeiroNomeObrigatorio()
+    })
 
-        Checkout.primeiroNomeVazio()
-        Checkout.continuar()
-        Checkout.validarNomeCheckout()
+    it('Exibir erro ao tentar avançar com o sobrenome em branco', () => {
+        Checkout.submeterFormularioSemSobrenome()
+        Checkout.avancarParaResumo()
+        Checkout.validarMensagemDeErroSobrenomeObrigatorio()
+    })
+
+    it('Exibir erro ao tentar avançar com o CEP em branco', () => {
+        Checkout.submeterFormularioSemCep()
+        Checkout.avancarParaResumo()
+        Checkout.validarMensagemDeErroCepObrigatorio()
     })
 
 })
